7a77e526986fd9517f8bb833372f1dcc512e41c6
sundayonah <sundayonah94@gmail.com>
2023-07-15 21:40:49 +0100

    done with popup.js for now

--- popup.js
+++ popup.js
@@ -97,7 +97,10 @@ let privateKey, address;
         })
     };
 
-    function openImport(){};
+    function openImport(){
+        document.getElementById('import_token').style.display = 'block';
+        document.getElementById('home').style.display = 'none';
+    };
 
     function getOpenNetwork(){
       document.getElementById('network').style.display = 'block';
@@ -198,35 +201,248 @@ let privateKey, address;
                     private_key: wallet.privateKey,
                     mnemonic: wallet.mnemonic.phrase,
                 };
+
+                const jsonObj = JSON.stringify(userWallet);
+                localStorage.setItem('userWallet', jsonObj);
+
+                document.getElementById('goHomePage').style.display = 'block';
+                window.location.reload();
+            })
+            .catch((err) =>{
+                console.log('error', err);ß
             })
         }
     };
 
-    function login(){};
+    function login(){
+        document.getElementById('login_form').style.display = 'none';
+        document.getElementById('center').style.display = 'block';
 
-    function logout(){};
+        const email = document.getElementById('login_email').value;
+        const password = document.getElementById('login_password').value;
 
-    function openTransfer(){};
+          //API
+          const url = 'https://localhost:3000/api/v1/user/login';
 
-    function goBack(){};
+          const data = {
+              email: email,
+              password: password,
+          };
 
-    function openActivity(){};
+          fetch(url,  {
+            method: 'POST',
+            handlers:{
+                'Content-Type': 'application/json',            
+            },
+            body: JSON.stringify(data),
+        }).then((response) => response.json()).then((result) =>{
+            console.log(result);
+      
+            const userWallet = {
+                address: wallet.address,
+                private_key: wallet.privateKey,
+                mnemonic: wallet.mnemonic.phrase,
+            };
 
-    function openAssets(){};
+            const jsonObj = JSON.stringify(userWallet);
+            localStorage.setItem('userWallet', jsonObj);
+            // document.getElementById('goHomePage').style.display = 'block';
+            window.location.reload();
+        }).catch((err) =>{
+            console.log('error', err);
+        })
 
-    function goHomePage(){};
+    };
 
-    function openImportModel(){};
+    function logout(){
+        localStorage.removeItem('userWallet');
+        window.location.reload();
+    };
 
-    function closeImportModel(){};
+    function openTransfer(){
+        document.getElementById('transfer_from').style.display = 'block';
+        document.getElementById('home').style.display = 'none';
+    };
 
-    function addToken(){};
+    function goBack(){
+        document.getElementById('transfer_from').style.display = 'none';
+        document.getElementById('home').style.display = 'block';
+    };
 
-    function addAccount(){};
+    function importGoBack(){
+        document.getElementById('import_token').style.display = 'nome';
+        document.getElementById('home').style.display = 'block';
+    }
 
-    function myFunction(){};
+    function openActivity(){
+        document.getElementById('activity').style.display = 'block';
+        document.getElementById('assets').style.display = 'none';
+    };
 
-    function copyAddress(){};
+    function openAssets(){
+        document.getElementById('activity').style.display = 'none';
+        document.getElementById('assets').style.display = 'block';
+    };
 
-    function changeAccount(){};
+    function goHomePage(){
+        document.getElementById('create_popUp').style.display = 'none';
+        document.getElementById('home').style.display = 'block';
+    };
+
+    function openImportModel(){
+        document.getElementById('import_account').style.display = 'block';
+        document.getElementById('home').style.display = 'none';
+    };
+
+    function closeImportModel(){
+        document.getElementById('import_account').style.display = 'none';
+        document.getElementById('home').style.display = 'block';
+    };
+
+    function addToken(){
+        const address = document.getElementById('token_address').value;
+        const name = document.getElementById('token_name').value;
+        const symbol = document.getElementById('token_symbol').value;
+
+        //API call
+
+        const url = 'https://localhost:3000/api/v1/token/createtoken';
+        const data = {
+            name: name,
+            address: address,
+            symbol: symbol
+        };
+
+        fetch(url, {
+            method: 'POST',
+            handlers:{
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        })
+        .then((response) => response.json())
+        .then((result) => {
+            console.log(result);
+            window.location.reload();
+        })
+        .catch((err) => {
+            console.log('error', err);
+        });
+    };
+
+    function addAccount(){
+        const privateKey = document.getElementById('add_account_private_key').value;
+        const provider = new ethers.providers.JsonRpcProvider(providerURL);
+        let wallet = new ethers.Wallet(privateKey, provider);
+
+        console.log(wallet)
+
+        const url = 'https://localhost:3000/api/v1/account/createaccount';
+
+        const data = {
+            privateKey: privateKey,
+            address: wallet.address,
+        };
+
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        })
+        .then((response) => response.json())
+        .then((result) => {
+            console.log(result);
+        })
+        .catch((err) =>{
+            console.log('Error', err)
+        })
+    };
+
+    function myFunction(){
+        const str = localStorage.getItem('userWallet');
+        const parseObj = JSON.parse(str);
+
+        if(parseObj.address){
+            document.getElementById('LoginUser').style.display = 'none';
+            document.getElementById('home').style.display = 'block';
+
+            privateKey = parseObj.private_key;
+            address = parseObj.address;
+
+            checkBalance(parseObj.address);
+        }
+
+        const tokenRender = document.querySelector('.assets');
+        const accountRender = document.querySelector('.accountList');
+        const url = 'https://localhost:3000/api/v1/tokens/alltoken'
+        fetch(url).then((response) => response.json()).then((data) => {
+            let elements = '';
+
+            data.data.tokens.map((token) => 
+            (elements +=
+                `<div class='assets_item'>
+                    <img class='assets_item_img'
+                    src='./assets/myFavicon.png'
+                    alt=''
+                    />
+                    <span>${token.address.slice(0, 15)}...</span>
+                    <span>${token.symbol}</span>
+                </div>`
+            ))
+            tokenRender.innerHTML = elements;
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+          
+        fetch('https://localhost:3000/api/v1/account/allaccount')
+        .then((response) => response.json())
+        .then((data) => {
+
+            let accounts = '';
+
+            data.data.accounts.map((account, i) => 
+            (accounts  +=
+                `<div class='list'>
+                    <p>${i + 1}</p>
+                    <p class='accountValue' 
+                    data-address=${account.address}
+                    data-privateKey=${account.privateKey}
+                    >
+                    ${account.address.slice(0, 25)}...</p>
+                </div>`
+            ))
+            accountRender.innerHTML = accounts;
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+        console.log(privateKey);
+    };
+
+    function copyAddress(){
+       navigator.clipboard.writeText(address);
+    };
+
+    function changeAccount(){
+      const data = document.querySelector('.accountValue');
+      const address = data.getAttribute('data-address');
+      const privateKey = data.getAttribute('data-privateKey');
+
+      console.log(privateKey, address);
+
+      const userWallet = {
+        address: address,
+        private_key: privateKey,
+        mnemonic: 'Changed',
+      };
+
+      const jsonObj = JSON.stringify(userWallet);
+      localStorage.setItem('userWallet', jsonObj);
+      window.location.reload();
+    };
+
+    window.onload = myFunction;
     
